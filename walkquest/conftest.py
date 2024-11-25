d3c1e2010573aaea9745afa047a668bf5f951f3c
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2024-11-25 16:28:43 +0000

    Add initial project files and configurations for the Walkquest project

---
+++ walkquest/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from walkquest.users.models import User
+from walkquest.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
