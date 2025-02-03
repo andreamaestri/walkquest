85cbdf13dfb3fd19b701f3851567bd8f32a2960e
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2025-02-03 19:27:44 +0000

    chore: Update .gitignore to include Tailwind source files and modify static file settings in base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -168,20 +168,19 @@ MIDDLEWARE = [
 
 # STATIC
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-root
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 STATIC_ROOT = str(BASE_DIR / "staticfiles")
-# https://docs.djangoproject.com/en/dev/ref/settings/#static-url
 STATIC_URL = "/static/"
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(APPS_DIR / "static"),
 ]
-# https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
 STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
-    "django_esm.finders.ESMFinder",
 ]
+
+# Disable Django's static file handling in development
+DEBUG_PROPAGATE_EXCEPTIONS = True
 
 # MEDIA
 # ------------------------------------------------------------------------------
