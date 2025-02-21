27828c2a5a1418ece430d85ac9b4da6b9cf1a263
Andrea Maestri <16215183+andreamaestri@users.noreply.github.com>
2025-02-21 08:40:32 +0000

    feat: Simplify entry point definition in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     outDir: resolve('./walkquest/static/dist'),
     rollupOptions: {
       input: {
-        'walkquest/static/js/main.js': resolve(__dirname, 'walkquest/static/js/main.js')
+        main: resolve(__dirname, 'walkquest/static/js/main.js')
       },
       output: {
         entryFileNames: '[name].[hash].js',
